fe7a8cccfbead70c944ee87cda2d411a48dce2ed
Alvin Wan <alvinwan1@gmail.com>
2020-04-03 21:02:36 -0700

    finished functioningg phone-based auth

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, redirect, url_for, render_template, jsonify
-from utils import send_sms
+from utils import send_sms, random_code
 app = Flask(__name__)
+
+
+code_to_phone_number = {}  # TODO: move this to db, def don't leave this here
 
 
 @app.route('/')
@@ -11,16 +14,28 @@ def hello_world():
 @app.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
-        send_sms(request.form['phone_number'], 'Here is your super duper secret code: HUEHUE')
+        code = random_code()
+        phone_number = request.form['phone_number']
+        send_sms(
+            phone_number=phone_number,
+            message=f'Here is your super duper secret code: {code}')
+        code_to_phone_number[code] = phone_number
         return redirect(url_for('code'))
-    return render_template('index.html')
+    return render_template('login.html')
 
 
 @app.route('/code', methods=('POST', 'GET'))
 def code():
     if request.method == 'POST':
+        code = request.form['code']
+        if code not in code_to_phone_number:
+            return jsonify({'success': False, 'message': 'nu'})
+
+        phone_number = code_to_phone_number.pop(code)
         return jsonify({
-            'Das': 'right' if request.form['code'] == 'HUEHUE' else 'wrong'
+            'success': True,
+            'message': 'yey',
+            'phone_number': phone_number
         })
     return render_template('code.html')
 
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import boto3
+import random
+import string
 
 sns_client = boto3.client('sns')  # TODO: how to auth on server
 
@@ -12,3 +14,12 @@ def send_sms(phone_number: str, message: str, **kwargs):
         Message=message,
         **kwargs
     )
+
+
+def random_code(stringLength=6):
+    """Generate a random string of letters and digits.
+
+    Totally copy-pasta'ed https://pynative.com/python-generate-random-string/
+    """
+    lettersAndDigits = string.ascii_letters + string.digits
+    return ''.join(random.choice(lettersAndDigits) for i in range(stringLength))
