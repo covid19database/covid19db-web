cc06c7c14540070b12f7e2326a477475a5df531c
Alvin Wan <alvinwan1@gmail.com>
2020-04-10 13:59:14 -0700

    splitting up edits into commits: login flow + home page

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 from flask import Flask, request, redirect, url_for, render_template, jsonify
+from flask_login import LoginManager, login_user, UserMixin, login_required, \
+    current_user
 from utils import send_sms, random_code
 import requests
 import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 API_BASE_URL = os.environ['API_BASE_URL']
 code_to_phone_number = {}  # TODO: move this to db, def don't leave this here
+
+
+class User(UserMixin):
+
+    def __init__(self, id):
+        self.id = id
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
 
 
 @app.route('/')
@@ -15,8 +32,16 @@ def index():
     return render_template('index.html', traces=traces.json())
 
 
+@app.route('/home')
+@login_required
+def home():
+    return render_template('home.html')
+
+
 @app.route('/login', methods=('GET', 'POST'))
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     if request.method == 'POST':
         code = random_code()
         phone_number = request.form['phone_number']
@@ -24,24 +49,23 @@ def login():
             phone_number=phone_number,
             message=f'Here is your super duper secret code: {code}')
         code_to_phone_number[code] = phone_number
-        return redirect(url_for('code'))
+        return redirect(url_for('code', phone_number=phone_number))
     return render_template('login.html')
 
 
 @app.route('/code', methods=('POST', 'GET'))
 def code():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     if request.method == 'POST':
         code = request.form['code']
         if code not in code_to_phone_number:
             return jsonify({'success': False, 'message': 'nu'})
 
         phone_number = code_to_phone_number.pop(code)
-        return jsonify({
-            'success': True,
-            'message': 'yey',
-            'phone_number': phone_number
-        })
-    return render_template('code.html')
+        login_user(User(phone_number))
+        return redirect(url_for('home'))
+    return render_template('code.html', phone_number=request.args.get('phone_number'))
 
 
 @app.route('/submit/', methods=('POST', 'GET'))
